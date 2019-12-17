dba7c4229e649602077987afdeebfe4305b50937
Oroken <34053443+Oroken@users.noreply.github.com>
2019-12-17 09:17:24 +0700

    Add files via upload

---
+++ DemoPipelineV1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demopipelinev1;
+
+/**
+ *
+ * @author Pham Minh Nhat
+ */
+public class DemoPipelineV1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void PipelineProblem() {
+        System.out.println("khó bỏ mịa");
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PipelineProblem();
+    }
+    
+}
